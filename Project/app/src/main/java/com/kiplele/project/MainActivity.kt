5a8055978a027da7c6900e14e11b919704f0d696
andrewkibet <61360677+andrewkibet@users.noreply.github.com>
2023-07-06 09:02:37 +0300

    Registers users with Firebase.
    Simple but now functional.

---
+++ Project/app/src/main/java/com/kiplele/project/MainActivity.kt
@@ -0,0 +1,118 @@
+package com.kiplele.project
+
+import android.app.Activity
+import android.content.Context
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.firebase.auth.FirebaseAuth
+import com.kiplele.project.ui.theme.ProjectTheme
+
+class MainActivity : ComponentActivity() {
+    private lateinit var auth:FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        auth = FirebaseAuth.getInstance()
+
+        setContent {
+            ProjectTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    RegistrationScreen()
+                }
+            }
+        }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun RegistrationScreen() {
+        val context= LocalContext.current
+        var name by remember { mutableStateOf("") }
+        var email by remember { mutableStateOf("") }
+        var password by remember {mutableStateOf("")
+
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TextField(
+                value = name,
+                onValueChange = { name = it },
+                label = { Text("Name") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            TextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Email") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            TextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Password") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = {
+                    // Handle registration button click
+                          registerUser(context,email,password)
+                },
+                modifier = Modifier.align(Alignment.End)
+            ) {
+                Text("Register")
+            }
+        }
+    }
+}
+private fun registerUser(context: Context, email: String, password: String) {
+    val auth = FirebaseAuth.getInstance()
+
+    auth.createUserWithEmailAndPassword(email, password)
+        .addOnCompleteListener(context as Activity) { task ->
+            if (task.isSuccessful) {
+                // Registration success, handle the registered user here
+            } else {
+                // Registration failed, handle the error here
+            }
+        }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun RegistrationScreenPreview() {
+    ProjectTheme {
+        MainActivity().RegistrationScreen()
+    }
+}
