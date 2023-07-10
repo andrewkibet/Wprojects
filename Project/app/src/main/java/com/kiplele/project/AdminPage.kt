0ed121d6e343a07acbbf69b232018737fa9a3960
andrewkibet <61360677+andrewkibet@users.noreply.github.com>
2023-07-10 09:53:40 +0300

    Update AdminPage.kt

--- Project/app/src/main/java/com/kiplele/project/AdminPage.kt
+++ Project/app/src/main/java/com/kiplele/project/AdminPage.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.toggleable
 import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
@@ -51,129 +53,132 @@ fun AdminPageContent() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Box(modifier = Modifier.fillMaxWidth()){
+
+        }
         DropdownMenu(
             expanded = false, // Set to true when the dropdown is open
             onDismissRequest = { /* Handle dismiss request */ },
             modifier = Modifier.fillMaxWidth()
         ) {
-            projectTypes.forEach { projectType ->
-                DropdownMenuItem(
-                    onClick = {
-                        selectedProjectType = projectType // Update the selected project type when clicked
-                    }
-                ) {
-                    Text(projectType)
-                }
-            }
-        }
+/* projectTypes.forEach { projectType ->
+     DropdownMenuItem(
+         onClick = {
+             selectedProjectType = projectType // Update the selected project type when clicked
+         }
+     ) {
+         Text(projectType)
+     }
+ }
 
-        Spacer(modifier = Modifier.height(16.dp))
-
-        TextField(
-            value = projectName,
-            onValueChange = { projectName = it },
-            label = { Text("Project Name") },
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        TextField(
-            value = tenderName,
-            onValueChange = { tenderName = it },
-            label = { Text("Tender Name") },
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        TextField(
-            value = tenderPhoneNumber,
-            onValueChange = { tenderPhoneNumber = it },
-            label = { Text("Tender Phone Number") },
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        TextField(
-            value = tenderEmail,
-            onValueChange = { tenderEmail = it },
-            label = { Text("Tender Email") },
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        TextField(
-            value = budget,
-            onValueChange = { budget = it },
-            label = { Text("Budget") },
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Button(
-            onClick = {
-                // Save the details in Firestore
-                saveDetailsToFirestore(
-                    context,
-                    selectedProjectType,
-                    projectName,
-                    tenderName,
-                    tenderPhoneNumber,
-                    tenderEmail,
-                    budget
-                )
-            },
-            modifier = Modifier.align(Alignment.End)
-        ) {
-            Text("Save")
-        }
-    }
+ */
 }
 
-fun DropdownMenuItem(onClick: () -> Unit, interactionSource: () -> Unit) {
+Spacer(modifier = Modifier.height(16.dp))
 
+TextField(
+ value = projectName,
+ onValueChange = { projectName = it },
+ label = { Text("Project Name") },
+ modifier = Modifier.fillMaxWidth()
+)
+
+Spacer(modifier = Modifier.height(16.dp))
+
+TextField(
+ value = tenderName,
+ onValueChange = { tenderName = it },
+ label = { Text("Tender Name") },
+ modifier = Modifier.fillMaxWidth()
+)
+
+Spacer(modifier = Modifier.height(16.dp))
+
+TextField(
+ value = tenderPhoneNumber,
+ onValueChange = { tenderPhoneNumber = it },
+ label = { Text("Tender Phone Number") },
+ modifier = Modifier.fillMaxWidth()
+)
+
+Spacer(modifier = Modifier.height(16.dp))
+
+TextField(
+ value = tenderEmail,
+ onValueChange = { tenderEmail = it },
+ label = { Text("Tender Email") },
+ modifier = Modifier.fillMaxWidth()
+)
+
+Spacer(modifier = Modifier.height(16.dp))
+
+TextField(
+ value = budget,
+ onValueChange = { budget = it },
+ label = { Text("Budget") },
+ modifier = Modifier.fillMaxWidth()
+)
+
+Spacer(modifier = Modifier.height(16.dp))
+
+Button(
+ onClick = {
+     // Save the details in Firestore
+     saveDetailsToFirestore(
+         context,
+         selectedProjectType,
+         projectName,
+         tenderName,
+         tenderPhoneNumber,
+         tenderEmail,
+         budget
+     )
+ },
+ modifier = Modifier.align(Alignment.End)
+) {
+ Text("Save")
 }
+}
+}
+
+
 
 private fun saveDetailsToFirestore(
-    context: Context,
-    projectType: String,
-    projectName: String,
-    tenderName: String,
-    tenderPhoneNumber: String,
-    tenderEmail: String,
-    budget: String
+context: Context,
+projectType: String,
+projectName: String,
+tenderName: String,
+tenderPhoneNumber: String,
+tenderEmail: String,
+budget: String
 ) {
-    val firestore = FirebaseFirestore.getInstance()
+val firestore = FirebaseFirestore.getInstance()
 
-    val data = hashMapOf(
-        "projectType" to projectType,
-        "projectName" to projectName,
-        "tenderName" to tenderName,
-        "tenderPhoneNumber" to tenderPhoneNumber,
-        "tenderEmail" to tenderEmail,
-        "budget" to budget
-    )
+val data = hashMapOf(
+"projectType" to projectType,
+"projectName" to projectName,
+"tenderName" to tenderName,
+"tenderPhoneNumber" to tenderPhoneNumber,
+"tenderEmail" to tenderEmail,
+"budget" to budget
+)
 
-    firestore.collection("adminProjects")
-        .add(data)
-        .addOnSuccessListener {
-            // Data saved successfully
-            Toast.makeText(context, "Details saved successfully", Toast.LENGTH_SHORT).show()
-        }
-        .addOnFailureListener {
-            // Error occurred while saving data
-            Toast.makeText(context, "Error saving details", Toast.LENGTH_SHORT).show()
-        }
+firestore.collection("adminProjects")
+.add(data)
+.addOnSuccessListener {
+ // Data saved successfully
+ Toast.makeText(context, "Details saved successfully", Toast.LENGTH_SHORT).show()
+}
+.addOnFailureListener {
+ // Error occurred while saving data
+ Toast.makeText(context, "Error saving details", Toast.LENGTH_SHORT).show()
+}
 }
 
 @Preview(showBackground = true)
 @Composable
 fun AdminPageContentPreview() {
-    ProjectTheme {
-        AdminPageContent()
-    }
+ProjectTheme {
+AdminPageContent()
+}
 }
