6a4c448fc9dca4ecbae42f9b820b11165d315cd2
andrewkibet <61360677+andrewkibet@users.noreply.github.com>
2023-07-10 14:53:08 +0300

    Update ProjectList.kt
    The code can now fetch data from the Firestore and Display them in theapp UI.

--- Project/app/src/main/java/com/kiplele/project/ProjectList.kt
+++ Project/app/src/main/java/com/kiplele/project/ProjectList.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -16,6 +21,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.google.firebase.firestore.FirebaseFirestore
 import com.kiplele.project.ui.theme.ProjectTheme
 
@@ -49,12 +55,64 @@ fun ProjectListContent() {
             // Display the fetched data
             Column {
                 projectListState.projectList.forEach { project ->
-                    Text(project.toString())
+                    ProjectFields(project)
+                    Spacer(modifier = Modifier.height(16.dp))
                 }
             }
         }
     }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProjectFields(project: Project) {
+    var projectType by remember { mutableStateOf(project.projectType) }
+    var projectName by remember { mutableStateOf(project.projectName) }
+    var tenderName by remember { mutableStateOf(project.tenderName) }
+    var tenderPhoneNumber by remember { mutableStateOf(project.tenderPhoneNumber) }
+    var tenderEmail by remember { mutableStateOf(project.tenderEmail) }
+    var budget by remember { mutableStateOf(project.budget) }
+
+    Column {
+        TextField(
+            value = projectType.orEmpty(),
+            onValueChange = { projectType = it },
+            label = { Text("Project Type") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        TextField(
+            value = projectName.orEmpty(),
+            onValueChange = { projectName = it },
+            label = { Text("Project Name") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        TextField(
+            value = tenderName.orEmpty(),
+            onValueChange = { tenderName = it },
+            label = { Text("Tender Name") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        TextField(
+            value = tenderPhoneNumber.orEmpty(),
+            onValueChange = { tenderPhoneNumber = it },
+            label = { Text("Tender Phone Number") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        TextField(
+            value = tenderEmail.orEmpty(),
+            onValueChange = { tenderEmail = it },
+            label = { Text("Tender Email") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        TextField(
+            value = budget.orEmpty(),
+            onValueChange = { budget = it },
+            label = { Text("Budget") },
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
+
 
 @Composable
 fun rememberProjectListState(): ProjectListState {
