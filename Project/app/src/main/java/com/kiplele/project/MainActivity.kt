681d67ab642cb56b810ebc04070e8d43bd694cbc
andrewkibet <61360677+andrewkibet@users.noreply.github.com>
2023-07-19 17:51:28 +0300

    Update MainActivity.kt

--- Project/app/src/main/java/com/kiplele/project/MainActivity.kt
+++ Project/app/src/main/java/com/kiplele/project/MainActivity.kt
@@ -3,16 +3,34 @@ package com.kiplele.project
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.kiplele.project.ui.theme.ProjectTheme
+import com.kiplele.project.ui.theme.Typography
+import com.kiplele.project.ui.ui.theme.Typography
 
 class MainActivity : ComponentActivity() {
+
+
+
+
+
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,25 +40,45 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            text = "Title",
+                           // style = MaterialTheme.typography.h4,
+                            modifier = Modifier.padding(top = 16.dp)
+                        )
+
+                        Image(
+                            painter = painterResource(id = R.drawable.walpaper),
+                            contentDescription = "Image",
+                            modifier = Modifier
+                                .padding(16.dp)
+                                .size(200.dp)
+                                .aspectRatio(1f)
+                        )
+
+                        Text(
+                            text = "Image Name",
+                         //   style = MaterialTheme.typography.body1,
+                            modifier = Modifier.padding(bottom = 16.dp)
+                        )
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DefaultPreview() {
     ProjectTheme {
-        Greeting("Android")
+        MainActivity()
     }
 }
