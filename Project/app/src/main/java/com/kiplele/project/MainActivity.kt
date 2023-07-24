9e7e3a143ecf2955e78b34ad69d70bcf39a9df4e
andrewkibet <61360677+andrewkibet@users.noreply.github.com>
2023-07-24 08:41:43 +0300

    Update MainActivity.kt

--- Project/app/src/main/java/com/kiplele/project/MainActivity.kt
+++ Project/app/src/main/java/com/kiplele/project/MainActivity.kt
@@ -1,21 +1,21 @@
 package com.kiplele.project
 
-import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -23,69 +23,120 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.kiplele.project.ui.theme.ProjectTheme
-import com.kiplele.project.ui.theme.Typography
-import com.kiplele.project.ui.ui.theme.Typography
 
 class MainActivity : ComponentActivity() {
-
-
-
-
-
-    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
-
-            Scaffold(){
-
-
-                ProjectTheme {
-                    // A surface container using the 'background' color from the theme
-                    Surface(
+            ProjectTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                )
+                {
+                    Column(
                         modifier = Modifier.fillMaxSize(),
-                        color = MaterialTheme.colorScheme.background
+                        horizontalAlignment = Alignment.CenterHorizontally // Left-align the elements
                     ) {
-                        Column(
-                            modifier = Modifier.fillMaxSize(),
-                            verticalArrangement = Arrangement.Center,
-                            horizontalAlignment = Alignment.CenterHorizontally
-                        ) {
-                            Text(
-                                text = "Title",
-                                // style = MaterialTheme.typography.h4,
-                                modifier = Modifier.padding(top = 16.dp)
-                            )
+                        Text(
+                            text = "Title",
+                            style = TextStyle(
+                                fontSize = 24.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = Color.Black
+                            ),
+                            modifier = Modifier.padding(top = 16.dp)
+                        )
 
-                            Image(
-                                painter = painterResource(id = R.drawable.walpaper),
-                                contentDescription = "Image",
-                                modifier = Modifier
-                                    .padding(16.dp)
-                                    .size(200.dp)
-                                    .aspectRatio(1f)
-                            )
+                        Image(
+                            painter = painterResource(id = R.drawable.walpaper),
+                            contentDescription = "Image",
+                            modifier = Modifier
+                                .padding(16.dp)
+                                .size(200.dp)
+                                .aspectRatio(1f)
+                        )
 
-                            Text(
-                                text = "Image Name",
-                                //   style = MaterialTheme.typography.body1,
-                                modifier = Modifier.padding(bottom = 16.dp)
+                        Text(
+                            text = "Image Name",
+                            style = TextStyle(
+                                fontSize = 16.sp,
+                                fontWeight = FontWeight.Normal,
+                                color = Color.Gray
+                            ),
+                            modifier = Modifier.padding(bottom = 16.dp)
+                        )
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        // Left-align the "Projects" text
+                        Text(
+                            text = "Projects",
+                            style = TextStyle(
+                                fontSize = 20.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = Color.Black
                             )
-                        }
+                        )
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        // Left-align the "Stakeholders" text
+                        Text(
+                            text = "Stakeholders",
+                            style = TextStyle(
+                                fontSize = 20.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = Color.Black
+                            )
+                        )
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        // Left-align the "News" text
+                        Text(
+                            text = "News",
+                            style = TextStyle(
+                                fontSize = 20.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = Color.Black
+                            )
+                        )
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        Text(
+                            text = "Projects",
+                            style = TextStyle(
+                                fontSize = 20.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = Color.Black
+                            ),
+                            modifier = Modifier
+                                .padding(16.dp)
+                                .size(200.dp)
+                                .aspectRatio(1f)
+                                .clickable {
+                                    val intent = Intent(
+                                        this@MainActivity,
+                                        ProjectList::class.java
+                                    )
+                                    startActivity(intent)
+                                }
+                        )
                     }
                 }
-
             }
-
-
         }
     }
 }
 
+@Composable
+fun UiContent(){
 
-
+}
 
 @Preview(showBackground = true)
 @Composable
