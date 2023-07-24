330ce5e6bdae625b2adf01354642a127c70bfe36
andrewkibet <61360677+andrewkibet@users.noreply.github.com>
2023-07-24 10:13:41 +0300

    Update LoginActivity.kt

--- Project/app/src/main/java/com/kiplele/project/LoginActivity.kt
+++ Project/app/src/main/java/com/kiplele/project/LoginActivity.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
@@ -74,6 +75,8 @@ class LoginActivity : ComponentActivity() {
                 label = { Text("Password") },
                 modifier = Modifier.fillMaxWidth()
             )
+            if (email.isEmpty() || password.isEmpty()) {
+                RedBulb()}
 
             Spacer(modifier = Modifier.height(16.dp))
 
@@ -102,6 +105,24 @@ class LoginActivity : ComponentActivity() {
                 }
             )
 
+        }
+    }
+
+    @Composable
+    fun RedBulb() {
+        Box(
+            modifier = Modifier
+                .size(24.dp)
+                .background(Color.Red, shape = MaterialTheme.shapes.small)
+        ) {
+            // Customize the red bulb icon as per your requirement
+            // For example, you can use an Icon composable here
+            // For now, we'll use a Text composable with a red bulb emoji
+            Text(
+                text = "🚨",
+                color = Color.White,
+                modifier = Modifier.align(Alignment.Center)
+            )
         }
     }
 
