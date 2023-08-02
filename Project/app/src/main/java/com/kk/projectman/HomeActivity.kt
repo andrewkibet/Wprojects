ddb1c26a4d2c34c2f811e29ddcd36614abdbcd20
andrewkibet <61360677+andrewkibet@users.noreply.github.com>
2023-08-02 13:40:41 +0300

    Update HomeActivity.kt

--- Project/app/src/main/java/com/kk/projectman/HomeActivity.kt
+++ Project/app/src/main/java/com/kk/projectman/HomeActivity.kt
@@ -1,11 +1,19 @@
 package com.kk.projectman
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var txvadmin: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        txvadmin = findViewById(R.id.admin)
+        txvadmin.setOnClickListener { val intent =Intent(this,Tendepreneurs:: class.java)
+        startActivity(intent)}
     }
 }
