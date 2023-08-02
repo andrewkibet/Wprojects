d6394a84eca9657031fe52155cba281800d85c31
andrewkibet <61360677+andrewkibet@users.noreply.github.com>
2023-08-02 10:23:05 +0300

    Update Tendepreneurs.kt

--- Project/app/src/main/java/com/kk/projectman/Tendepreneurs.kt
+++ Project/app/src/main/java/com/kk/projectman/Tendepreneurs.kt
@@ -1,7 +1,16 @@
 package com.kk.projectman
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.kk.projectman.databinding.ActivityMainBinding
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
 class Tendepreneurs : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
