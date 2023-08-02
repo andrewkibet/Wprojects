2d908f8540ae0d909a13db5a613ef1338fd7b707
andrewkibet <61360677+andrewkibet@users.noreply.github.com>
2023-08-02 14:48:56 +0300

    change most of the code

--- Project/app/src/main/java/com/kk/projectman/Tendepreneurs.kt
+++ Project/app/src/main/java/com/kk/projectman/Tendepreneurs.kt
@@ -1,27 +1,21 @@
-package com.kk.projectman
-
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.kk.projectman.databinding.ActivityMainBinding
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
+import com.kk.projectman.R
 
 class Tendepreneurs : AppCompatActivity() {
-    private lateinit  var fab: FloatingActionButton
-
+    private lateinit var fab: FloatingActionButton
 
     private lateinit var storageRef: StorageReference
     private lateinit var firebaseFirestore: FirebaseFirestore
     private var imageUri: Uri? = null
-//    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,7 +23,57 @@ class Tendepreneurs : AppCompatActivity() {
 
         fab = findViewById(R.id.fab)
 
+        // Initialize Firebase Firestore and Firebase Storage
+        firebaseFirestore = FirebaseFirestore.getInstance()
+        storageRef = FirebaseStorage.getInstance().reference
 
+        // Set a click listener to the FAB
+        fab.setOnClickListener {
+            // Launch image picker to select an image
+            pickImage.launch("image/*")
+        }
     }
 
+    // Activity result contract for image picking
+    private val pickImage = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+        if (uri != null) {
+            // Image picked, save the URI
+            imageUri = uri
+            // Upload the image to Firestore Storage
+            uploadImageToStorage()
+        } else {
+            // Image picking was canceled or failed
+            Toast.makeText(this, "Image picking failed", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun uploadImageToStorage() {
+        if (imageUri != null) {
+            val imageRef = storageRef.child("images/${imageUri?.lastPathSegment}")
+
+            imageRef.putFile(imageUri!!)
+                .addOnSuccessListener { taskSnapshot ->
+                    // Image uploaded successfully, get the download URL
+                    imageRef.downloadUrl.addOnSuccessListener { downloadUri ->
+                        // The download URL of the image
+                        val imageUrl = downloadUri.toString()
+
+                        // Now you can use the imageUrl to store it in Firestore or perform any other action
+                        // For example, you can save the URL to Firestore as follows:
+                        saveImageUrlToFirestore(imageUrl)
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    // Handle the error if the image upload fails
+                    Toast.makeText(this, "Image upload failed", Toast.LENGTH_SHORT).show()
+                }
+        }
+    }
+
+    private fun saveImageUrlToFirestore(imageUrl: String) {
+        // Here you can save the imageUrl to Firestore along with other data
+        // For example, you can use firebaseFirestore.collection("tendepreneurs").document().set(...)
+        // to store the imageUrl and other data in Firestore.
+        // Implement the logic according to your use case.
+    }
 }
