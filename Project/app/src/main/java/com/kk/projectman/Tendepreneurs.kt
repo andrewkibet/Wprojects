eca7e52786b13497025df8f48f8356c64f733cfd
andrewkibet <61360677+andrewkibet@users.noreply.github.com>
2023-08-02 10:42:11 +0300

    Update Tendepreneurs.kt

--- Project/app/src/main/java/com/kk/projectman/Tendepreneurs.kt
+++ Project/app/src/main/java/com/kk/projectman/Tendepreneurs.kt
@@ -7,14 +7,19 @@ import android.view.View
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.kk.projectman.databinding.ActivityMainBinding
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 
 class Tendepreneurs : AppCompatActivity() {
+    private lateinit  var fab: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tendepreneurs)
+
+        fab = findViewById(R.id.fab)
     }
 }
