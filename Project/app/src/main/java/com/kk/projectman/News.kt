4c9c979dfc14fda67fac96002c419b6c3490b158
andrewkibet <61360677+andrewkibet@users.noreply.github.com>
2023-08-03 11:44:57 +0300

    Update News.kt

--- Project/app/src/main/java/com/kk/projectman/News.kt
+++ Project/app/src/main/java/com/kk/projectman/News.kt
@@ -6,13 +6,17 @@ import android.os.Handler
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import com.kk.projectman.R
+import android.content.Intent
+import android.net.Uri
+
+
 
 class News : AppCompatActivity() {
 
     private lateinit var imageView: ImageView
     private val handler = Handler()
     private var currentImageIndex = 0
-    private val imageResources = intArrayOf(R.drawable.bg12, R.drawable.bg3, R.drawable.bg)
+    private val imageResources = intArrayOf(R.drawable.rrgath, R.drawable.ltm, R.drawable.bg)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,4 +51,11 @@ class News : AppCompatActivity() {
         // Increment the current image index for the next update
         currentImageIndex = (currentImageIndex + 1) % imageResources.size
     }
+
+    private fun openUrlInBrowser(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(url)
+        startActivity(intent)
+    }
+
 }
