2296eb18dab3bf2e026fd8822d8b767d578b7aa9
andrewkibet <61360677+andrewkibet@users.noreply.github.com>
2023-08-03 10:10:28 +0300

    Update News.kt

--- Project/app/src/main/java/com/kk/projectman/News.kt
+++ Project/app/src/main/java/com/kk/projectman/News.kt
@@ -2,8 +2,17 @@ package com.kk.projectman
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+
 
 class News : AppCompatActivity() {
+
+    private lateinit var imageView: ImageView
+    private val handler = Handler()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
