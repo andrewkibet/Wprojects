c036f93f775187c24dd8f08245ceb6c8d6d24691
andrewkibet <61360677+andrewkibet@users.noreply.github.com>
2023-08-03 13:44:16 +0300

    Update HomeActivity.kt

--- Project/app/src/main/java/com/kk/projectman/HomeActivity.kt
+++ Project/app/src/main/java/com/kk/projectman/HomeActivity.kt
@@ -3,11 +3,13 @@ package com.kk.projectman
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var txvadmin: TextView
+    private lateinit var button: Button
     private lateinit var textview: TextView
     private lateinit var news: TextView
     private lateinit var stakeholders: TextView
@@ -15,6 +17,10 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        button = findViewById(R.id.logout)
+        button.setOnClickListener { val intent = Intent (this,LoginActivity::class.java)
+            startActivity(intent) }
 
         news =  findViewById(R.id.newstv)
         news.setOnClickListener { val intent = Intent (this,News::class.java)
