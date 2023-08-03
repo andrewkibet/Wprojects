5ef3e96287fa49d3dc34a43f8ef916baf18c5eb2
andrewkibet <61360677+andrewkibet@users.noreply.github.com>
2023-08-03 10:43:35 +0300

    Update News.kt

--- Project/app/src/main/java/com/kk/projectman/News.kt
+++ Project/app/src/main/java/com/kk/projectman/News.kt
@@ -5,24 +5,25 @@ import android.os.Bundle
 import android.os.Handler
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
-
+import com.kk.projectman.R
 
 class News : AppCompatActivity() {
 
     private lateinit var imageView: ImageView
     private val handler = Handler()
-
+    private var currentImageIndex = 0
+    private val imageResources = intArrayOf(R.drawable.bg12, R.drawable.bg3, R.drawable.bg)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
-
 
         imageView = findViewById(R.id.imgv3)
 
         // Start periodic updates every 5 seconds (5000 milliseconds)
         startPeriodicUpdates()
     }
+
     private fun startPeriodicUpdates() {
         handler.postDelayed(object : Runnable {
             override fun run() {
@@ -34,14 +35,8 @@ class News : AppCompatActivity() {
             }
         }, 5000) // Initial delay of 5 seconds
     }
+
     private fun updateImageView() {
-        // Perform any necessary actions to fetch the new image resource
-        // For example, you can use an image loading library like Glide or Picasso
-
-        // In this example, let's assume we have an array of image resources and a counter to keep track of the current image index
-        val imageResources = arrayOf(R.drawable.bg12, R.drawable.bg3, R.drawable.bg)
-        var currentImageIndex = 0
-
         // Set the new image resource
         imageView.setImageResource(imageResources[currentImageIndex])
 
@@ -52,5 +47,4 @@ class News : AppCompatActivity() {
         // Increment the current image index for the next update
         currentImageIndex = (currentImageIndex + 1) % imageResources.size
     }
-
 }
