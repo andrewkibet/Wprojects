726e21f9e1184e38ff852bcbaf43291e2127a45a
andrewkibet <61360677+andrewkibet@users.noreply.github.com>
2023-08-09 09:50:46 +0300

    Update ProjectList.kt

--- Project/app/src/main/java/com/kk/projectman/ProjectList.kt
+++ Project/app/src/main/java/com/kk/projectman/ProjectList.kt
@@ -42,6 +42,7 @@ class ProjectList : ComponentActivity() {
 
                         )
                         ProjectListContent()
+                        TendepreneurListContent()
 
                     }}
 
